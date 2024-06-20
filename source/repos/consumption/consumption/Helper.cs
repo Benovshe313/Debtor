57bbf9da76e94496fde727a001d644dc57745635
Benovshe313 <b.mahalova@gmail.com>
2024-06-20 22:52:19 +0400

    File

---
+++ source/repos/consumption/consumption/Helper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumption
+{
+    internal class Helper
+    {
+        public static double FindDailyNorm(double weight)
+        {
+            return weight / 20;
+        }
+
+        //public static void SaveDailyConsumption(DateTime date, double amount)
+        //{
+        //    string record = $"{date.ToString("dd/MM/yyyy")}:{amount}";
+        //    File.AppendAllLines(@"C:\Users\User\Desktop\water_consumption.txt", new[] { record });
+
+
+        //}
+        public static void SaveDailyConsumption(DateTime date, double amount)
+        {
+            string dateStr = date.ToString("dd/MM/yyyy");
+            List<string> lines = new List<string>();
+            bool dateExists = false;
+
+            if (File.Exists(@"C:\Users\User\Desktop\water_consumption.txt"))
+            {
+                lines.AddRange(File.ReadAllLines(@"C:\Users\User\Desktop\water_consumption.txt"));
+            }
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] parts = lines[i].Split(':');
+                if (parts[0] == dateStr)
+                {
+                    double waterIntake = double.Parse(parts[1]) + amount;
+                    lines[i] = $"{dateStr}:{Math.Round(waterIntake, 2)}";
+                    dateExists = true;
+                    break;
+                }
+            }
+
+            if (!dateExists)
+            {
+                lines.Add($"{dateStr}:{amount}");
+            }
+
+            File.WriteAllLines(@"C:\Users\User\Desktop\water_consumption.txt", lines);
+        }
+
+        public static double GetTodayWaterIntake(DateTime date)
+        {
+            if (File.Exists(@"C:\Users\User\Desktop\water_consumption.txt"))
+            {
+                string[] lines = File.ReadAllLines(@"C:\Users\User\Desktop\water_consumption.txt");
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(':');
+                    if (parts[0] == date.ToString("dd/MM/yyyy"))
+                    {
+                        return double.Parse(parts[1]);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public static void PrintAllRecords()
+        {
+            if (File.Exists(@"C:\Users\User\Desktop\water_consumption.txt"))
+            {
+                string[] lines = File.ReadAllLines(@"C:\Users\User\Desktop\water_consumption.txt");
+                Console.WriteLine("Date\t\tWater Intake (l)");
+                Console.WriteLine("--------------------------------");
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(':');
+                    if (parts.Length == 2)
+                    Console.WriteLine($"{parts[0]}\t{parts[1]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No records found.");
+            }
+        }
+
+        public static void SaveNameAndWeight(string filePath, ref string name, ref double weight)
+        {
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Weight: ");
+            weight = User.validWeight();
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine(name);
+                sw.WriteLine(weight);
+            }
+        }
+    }
+}
---
+++ source/repos/consumption/consumption/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace consumption
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\User\Desktop\water_consumption.txt";
+            string name = "";
+            double weight = 0;
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    name = sr.ReadLine();
+                    double.TryParse(sr.ReadLine(), out weight);
+                }
+            }
+            else
+            {
+                Helper.SaveNameAndWeight(filePath, ref name, ref weight);
+            }
+
+
+
+            Console.WriteLine($"Welcome {name}!");
+
+        
+            DateTime currentDate = DateTime.Now;
+            
+            
+
+            while (true)
+            {
+                var todayDrunk2 = Helper.GetTodayWaterIntake(currentDate);
+
+                Console.Write("Date: ");
+                Console.WriteLine(currentDate.ToString("dd/MM/yyyy"));
+
+
+                Console.WriteLine($"Daily norm: {Helper.FindDailyNorm(weight)} l");
+                Console.WriteLine($"Today: {todayDrunk2} l");
+
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Drink water");
+                Console.WriteLine("2. History");
+                Console.WriteLine("3. End of day");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Make choice: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.WriteLine("Enter amount of water you drink: ");
+                    double todayInput = User.validAmount();
+                    User.todayDrunk += todayInput;
+                    Helper.SaveDailyConsumption(currentDate, User.todayDrunk);
+
+                }
+                else if (choice == "2")
+                {
+                    Helper.PrintAllRecords();
+
+                    //Console.WriteLine(currentDate.ToString("dd/MM/yyyy"));
+                    //Console.WriteLine($"Amount of water you drank: {User.todayDrunk} l");
+
+                    //if(currentDate == currentDate.AddDays(1))
+                    //{
+                    //    Console.WriteLine(currentDate.AddDays(1).ToString("dd/MM/yyyy"));
+                    //    Console.WriteLine($"Amount of water you drank: {User.NextDayDrunk} l");
+                    //}
+
+                    Console.ReadKey();
+                }
+                else if (choice == "3")
+                {
+                    if (Helper.FindDailyNorm(weight) < User.todayDrunk)
+                    {
+                        Console.WriteLine("You drank more than norm");
+                    }
+                    else if (Helper.FindDailyNorm(weight) > User.todayDrunk)
+                    {
+                        Console.WriteLine("You didn't drink enough water");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You drank enough water");
+                    }
+                    //Helper.SaveDailyConsumption(currentDate, User.todayDrunk);
+                    currentDate = currentDate.AddDays(1);
+                    User.todayDrunk = 0;
+
+                }
+                else if (choice == "4") 
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Make a choice again");
+                }
+
+            }
+        }
+    }
+
+}
